8b6242ba9051df40a736cd5a25a63c809b618472
mbejda <mbejda@live.com>
2013-12-07 16:08:09 -0500

    fixed coupon scheme

--- app/models/coupon.js
+++ app/models/coupon.js
@@ -2,45 +2,19 @@ var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 var Email = mongoose.SchemaTypes.Email;
 
-var PublisherSchema = new Schema({
-  name: { type: String},
-  token: { type: String},
-  secretToken: { type: String}
-});
-
 var CouponSchema = new Schema({
   // eMail address
-  name: { type: Email, unique: true },
-  account_id
+  start_date : {type:String},
+  end_date : {type:String},
+  promote : {type:String},
+  store_name: { type: String },
+  image_url : {type:String},
+  store_address : {city:{type:String}, state:{type:String}, Zip:{type:Number}},
+  store_contact : {email : {type:String},phone:{type:String}},
+  image :  {type:String},
+  creator_id : {type:String}
 
 });
 
 
-
-
-
-AccountSchema.method('checkPassword', function (password, callback) {
-});
-
-
-AccountSchema.static('authenticate', function (email, password, callback) {
-  this.findOne({ email: email }, function(err, user) {
-    if (err)
-      return callback(err);
-
-    if (!user)
-      return callback(null, false);
-
-    user.checkPassword(password, function(err, passwordCorrect) {
-      if (err)
-        return callback(err);
-
-      if (!passwordCorrect)
-        return callback(null, false);
-
-      return callback(null, user);
-    });
-  });
-});
-
-module.exports = mongoose.model('Account', AccountSchema);
+module.exports = mongoose.model('Coupons', CouponSchema);
